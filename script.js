9b4aefa1a614365c9a00aac2168114d691895f25
Karla Ortiz <karla.ortiz@codenation.org>
2023-07-25 11:53:22 -0700

    comments added to code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//
+//variables - grabbing the form from index.html
 const contactForm = document.querySelector('#contactUsForm');
 
+//Event Listener - prevents the default and the data is saved when the submit button is clicked into the internal server and contactData.json - triggers the event
 contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -16,13 +17,16 @@ contactForm.addEventListener('submit', (event) => {
     message,
   };
 
+//post request using the axios library - it alerts the user if their data was saved or not to our internal backend server
   axios
+  //information saved successfully
     .post('http://localhost:3001/contact', formData)
     .then((response) => {
       console.log(response.data.message);
       alert('Contact information submitted successfully! Be on the lookout for an email from A&E Handyman!');
       contactForm.reset();
     })
+    //information error
     .catch((error) => {
       console.error(error);
       alert('Failed to submit contact information. Please try again.');
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//libraries
+//libraries imported
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
+const morgan = require('morgan'); //logs http req and res
+const fs = require('fs'); //allows interacting with files using node.js
+const path = require('path'); //works with file and directory paths
 
 const PORT = process.env.PORT || 3001
 
@@ -13,11 +13,11 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false})); //makes data availabe in body
 
-const contactsFilePath = path.join(__dirname, 'contactsData.json');
+const contactsFilePath = path.join(__dirname, 'contactsData.json'); //defines path for saving data - data is saved in contactData.json file
 
-//Save contact information to internal server
+//Save contact information to internal server - defines the steps needed to save the data in a specific path and how it should look
 app.post('/contact', (req, res)=>{
   const { fullName, email, phoneNumber, message } = req.body;
   const data = {
@@ -28,7 +28,7 @@ app.post('/contact', (req, res)=>{
     timestamp: new Date().toISOString(),
   };
 
-  
+//pushes the data into the array in contactsData.json  
 fs.readFile(contactsFilePath, 'utf8', (err, existingData) => {
   if (err && err.code !== 'ENOENT') {
     console.error(err);
