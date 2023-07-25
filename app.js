44493ad7b5876da13339bd10ad5e634b96ea8a98
Karla Ortiz <karla.ortiz@codenation.org>
2023-07-24 21:32:05 -0700

    data is saved on a local json file

---
+++ app.js
@@ -0,0 +1,3 @@
+//Variables
+
+//Event Listeners
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 
+const contactsFilePath = path.join(__dirname, 'contactsData.json');
+
 //Save contact information to internal server
 app.post('/contact', (req, res)=>{
   const { fullName, email, phoneNumber, message } = req.body;
@@ -25,21 +27,31 @@ app.post('/contact', (req, res)=>{
     message,
     timestamp: new Date().toISOString(),
   };
-  fs.appendFile(
-    path.join(__dirname, 'contacts.txt'),
-    JSON.stringify(data) + '\n',
-    (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Failed to save the data.' });
-      }
-      console.log('Data saved successfully:', data);
-      res.status(200).json({ message: 'Data saved successfully.' });
+
+  
+fs.readFile(contactsFilePath, 'utf8', (err, existingData) => {
+  if (err && err.code !== 'ENOENT') {
+    console.error(err);
+    return res.status(500).json({ error: 'Failed to read the data file.' });
+  }
+
+  const contactsData = existingData ? JSON.parse(existingData) : [];
+  contactsData.push(data);
+
+  // Write the updated data to the file
+  fs.writeFile(contactsFilePath, JSON.stringify(contactsData, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to save the data.' });
     }
-  );
+    console.log('Data saved successfully:', data);
+    res.status(200).json({ message: 'Data saved successfully.' });
+    });
+  });
 });
+
 
 //PORT listener
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
-})
+});
